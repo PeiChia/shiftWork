78ef520fe0b98338202135e597ee641d5dcb0ed7
邱珮嘉 <bridget.c@inwinstack.com>
2022-07-14 11:25:23 +0800

    add: navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <b-navbar type="dark" style="background-color: #f03b0e">
+      <b-navbar-brand class="mx-auto"><b>排班小工具</b></b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
