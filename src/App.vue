74ccc904d302709489307931e1641490750e4deb
邱珮嘉 <bridget.c@inwinstack.com>
2022-07-14 11:23:34 +0800

    remove: related helloworld example

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar />
     <router-view />
   </div>
 </template>
@@ -13,7 +10,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
@@ -30,3 +27,12 @@
   color: #42b983;
 }
 </style>
+
+<script>
+import NavBar from "@/components/Navbar.vue";
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
